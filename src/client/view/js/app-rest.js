000863b624e713bccddcb0a3d460514401c54786
gael leroy <leroy-gael@outlook.com>
2016-12-02 18:00:22 +0100

    App REST : creation joueur et authentification

---
+++ src/client/view/js/app-rest.js
@@ -0,0 +1,138 @@
+var submitCreateAccountForm = document.getElementById('submitCreateAccountForm');
+var alertCreateAccountForm = document.getElementById('alertCreateAccountForm');
+var usernameCreateAccountForm = document.getElementById('usernameCreateAccountForm');
+var emailCreateAccountForm = document.getElementById('emailCreateAccountForm');
+var pwdCreateAccountForm = document.getElementById('pwdCreateAccountForm');
+var verifyPwdCreateAccountForm = document.getElementById('verifyPwdCreateAccountForm');
+
+var submitAuthenticate = document.getElementById('submitAuthenticate');
+var alertAuthenticateForm = document.getElementById('alertAuthenticateForm');
+var pwdAuthenticateForm = document.getElementById('pwdAuthenticateForm');
+var emailAuthenticateForm = document.getElementById('emailAuthenticateForm');
+
+
+
+
+submitCreateAccountForm.addEventListener("click", function(event){
+    event.preventDefault();
+    checkCreateAccountForm();
+});
+
+
+submitAuthenticateForm.addEventListener("click", function(event){
+    event.preventDefault();
+    checkAuthenticateForm();
+});
+
+
+
+
+
+
+
+
+function checkCreateAccountForm(){
+    alertCreateAccountForm.innerText ="";
+    if(isEmpty(usernameCreateAccountForm.value)){
+        alertCreateAccountForm.innerText ="The field username can't be empty";
+        return;
+    }
+    if(!validateEmail(emailCreateAccountForm.value)){
+        alertCreateAccountForm.innerText ="Please enter a valid email";
+        return;
+    }
+    if(isEmpty(pwdCreateAccountForm.value)){
+        alertCreateAccountForm.innerText ="The field password can't be empty";
+        return;
+    }
+    if(pwdCreateAccountForm.value !== verifyPwdCreateAccountForm.value){
+        alertCreateAccountForm.innerText ="The password fields don't match";
+        return;
+    }
+
+    registerUser(usernameCreateAccountForm.value, emailCreateAccountForm.value, pwdCreateAccountForm.value);
+   
+}
+
+
+
+function checkAuthenticateForm(){
+    alertAuthenticateForm.innerText ="";
+    if(!validateEmail(emailAuthenticateForm.value)){
+        alertAuthenticateForm.innerText ="Please enter a valid email";
+        return;
+    }
+    if(isEmpty(pwdAuthenticateForm.value)){
+        alertAuthenticateForm.innerText ="The field password can't be empty";
+        return;
+    }
+
+    authenticate(emailAuthenticateForm.value, pwdAuthenticateForm.value);
+}
+
+
+
+
+
+
+function registerUser(username, email, pwd){
+    alertCreateAccountForm.innerText ="";
+
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+           if (xmlhttp.status == 200) {
+               alertCreateAccountForm.innerText =xmlhttp.responseText;
+               //should change the class of alertCreateAccountForm to display it in a positive color
+           }
+           else {
+               alertCreateAccountForm.innerText =xmlhttp.responseText;
+           }
+        }
+    };
+
+    xmlhttp.open("POST", "http://localhost:8080/registerUser");
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.send(JSON.stringify({username: username, pwd: pwd, email: email}));
+
+}
+
+
+
+function authenticate(email, pwd){
+    alertAuthenticateForm.innerText ="";
+
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+           if (xmlhttp.status == 200) {
+                document.getElementById('formsContainer').className = 'closed';
+                document.getElementById('lobbyContainer').className = 'open';
+                console.log(JSON.parse(xmlhttp.responseText));
+           }
+           else {
+               alertAuthenticateForm.innerText =xmlhttp.responseText;
+           }
+        }
+    };
+
+    xmlhttp.open("POST", "http://localhost:8080/checkUserCredentials");
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.send(JSON.stringify({pwd: pwd, email: email}));
+
+}
+
+
+
+
+function isEmpty(s){
+    return (s === null || s.trim() === '');
+}
+
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
--- src/server/server.js
+++ src/server/server.js
@@ -11,8 +11,12 @@ module.exports.listen = function () {
     let app = require('express')();
     let server = require('http').Server(app);
     let io = require('socket.io')(server);
+    let bodyParser = require('body-parser');
 
     server.listen(props.socketProps.port);
+
+    // parse application/json
+    app.use(bodyParser.json());
 
     // init Db connection
 
@@ -24,15 +28,34 @@ module.exports.listen = function () {
                 console.log(err.message);
                 res.status(503);
             }
+            else{
+                next();
+            }
         });
-        next();
 
     });
 
 
     app.post("/registerUser", function (req,res) {
-        let email = req.body.userName;
-        let pwd = req.body.pwd;
+        mongoDb.registerUser(req.body, (err) =>{
+            if(err){
+                res.status(409).send(err.message);
+            }
+            else{
+                res.status(200).send('Successfully created');
+            }
+        });
+    });
+
+    app.post("/checkUserCredentials", function (req,res){
+        mongoDb.checkUserCredentials(req.body, (err, user) =>{
+            if(err){
+                res.status(422).send(err.message);
+            }
+            else{
+                res.status(200).send(user);
+            }
+        });
     });
 
     app.get("/scores" , function (req,res) {
