fbc6138ef90bcaf36b154775c8519acca280f18f
jy95 <jacques.yakoub@hotmail.com>
2016-12-11 23:50:52 +0100

    Fix du commit https://github.com/jy95/P4ng/commit/b57323b0fe91067a417804f0ad48474771b37f90 PS: Quand on change quelque chose , on se pose des questions avant

--- src/server/server.js
+++ src/server/server.js
@@ -63,7 +63,7 @@ module.exports.listen = function () {
 
     let gameEventEmitter = require("./server-logic/gameEventEmitter.js").commonEmitter;
 
-    gameEventEmitter.on( eventsEnum.gameStateUpdate, function (data) {
+    gameEventEmitter.on( eventsEnum.playerStateUpdate, function (data) {
 
         require("./server-logic/routes.js").gameStateUpdate(data);
 
